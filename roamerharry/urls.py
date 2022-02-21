843f2065f20e18a7578bf2d2c1483eabf119f260
harryhmx <harry4769@gmail.com>
2022-02-21 17:53:32 +0800

    Config urls for static and media files & add static default image

--- roamerharry/urls.py
+++ roamerharry/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from archive import views as av
@@ -23,3 +25,6 @@ urlpatterns = [
     # Config urls for homepage and apps
     path('', av.index, name='home'),
 ]
+
+# Config url for image upload
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
