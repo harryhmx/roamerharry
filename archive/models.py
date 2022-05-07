e9dae9a9339748040a22756bd31fe7dd0f1b2a30
harryhmx <harry4769@gmail.com>
2022-05-07 21:30:09 +0800

    Config models & admin

--- archive/models.py
+++ archive/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import models as um
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(um.User, on_delete=models.SET_NULL, related_name='blogs', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    slug = models.SlugField()
+    body = models.TextField()
+    hits = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
